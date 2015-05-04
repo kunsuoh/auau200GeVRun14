f7c10af57019a996bcb059cbcf8a1b164e66387e
kunsuoh <kunsuoh@gmail.com>
2015-05-04 17:27:45 +0900

    fix bug

--- submit_Ana.sh
+++ submit_Ana.sh
@@ -3,7 +3,7 @@ count=0
 for filename in $(cat fileLists/AnaList); do
    count2=$(($count/100))
    if [ -f Out/$filename.picoNpe.root ]; then
-      if [ $(($count%100)) -eq 0 ]; then 
+      if [ $(($count%100)) -eq "0" ]; then 
          cp ./run.csh ./run_Ana_$count2.csh
       fi
       echo "root4star -l -b <<EOF">>run_Ana_$count2.csh
@@ -13,7 +13,7 @@ for filename in $(cat fileLists/AnaList); do
       echo "EOF">>run_Ana_$count2.csh
       #qsub -hard -l h_vmem=4G -l scratchfree=500,gscratchio=1,projectio=1 -o Log/job_Ana_$filename.log -e Log/job_Ana_$filename.err run_Ana_$count2.csh
       
-      if [ $(($count%100)) -eq 99 ]; then 
+      if [ $(($count%100)) -eq "99" ]; then 
          ./run_Ana_$count2.csh
       fi
       mv run_Ana_$count2.csh script/. 
