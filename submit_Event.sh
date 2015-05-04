1003ed79c0f61009ffc907c3ab4eac972d78bcdd
kunsuoh <kunsuoh@gmail.com>
2015-05-04 20:05:14 +0900

    submit Event!

--- submit_Event.sh
+++ submit_Event.sh
@@ -16,6 +16,6 @@ for filename in $(cat fileLists/Run14/AuAu/200GeV/picoLists/runs/picoList_$1.lis
    mv $1_$count.list script/
    count=$(($count+1))
 done   
-#qsub -hard -l h_vmem=4G -l scratchfree=500,gscratchio=1,projectio=1 -o Log/job_$1.log -e Log/job_$1.err run_$1.csh
+qsub -hard -l h_vmem=4G -l scratchfree=500,gscratchio=1,projectio=1 -o Log/job_$1.log -e Log/job_$1.err run_$1.csh
 mv run_$1.csh script/
 
