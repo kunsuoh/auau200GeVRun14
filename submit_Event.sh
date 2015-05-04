7a61220c77db806651cc12ca6a6c7cefb37683f1
kunsuoh <kunsuoh@gmail.com>
2015-05-04 14:44:58 +0900

    make picoNpeLists

--- submit_Event.sh
+++ submit_Event.sh
@@ -5,7 +5,7 @@ count=0
 cp ./run.csh ./run_$1.csh
 for filename in $(cat fileLists/Run14/AuAu/200GeV/picoLists/runs/picoList_$1.list); do
    echo $filename >> $1_$count.list
-   
+   echo Out/$1_$count >> fileLists/Run14/AuAu/200GeV/picoNpeLists/runs/$1_$count.list
    echo "root4star -l -b <<EOF">>run_$1.csh
    echo -n ".x runPicoNpeEventMaker.C(\"script/">>run_$1.csh
    echo -n $1_$count.list>>run_$1.csh
