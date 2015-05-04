9d60013d6524d601f9df3dfcf8aa8341a4a98479
kunsuoh <kunsuoh@gmail.com>
2015-05-04 17:32:13 +0900

    for qsub

--- submit_Ana.sh
+++ submit_Ana.sh
@@ -12,11 +12,10 @@ for filename in $(cat fileLists/AnaList); do
       echo -n $filename>>run_Ana_$count2.csh
       echo  "\")">>run_Ana_$count2.csh
       echo "EOF">>run_Ana_$count2.csh
-      #qsub -hard -l h_vmem=4G -l scratchfree=500,gscratchio=1,projectio=1 -o Log/job_Ana_$filename.log -e Log/job_Ana_$filename.err run_Ana_$count2.csh
-      
       if [ $(($count%100)) -eq "99" ]; then 
          echo "Submit run_Ana_$count2.csh" 
-         ./run_Ana_$count2.csh
+         #./run_Ana_$count2.csh
+         qsub -hard -l h_vmem=4G -l scratchfree=500,gscratchio=1,projectio=1 -o Log/job_Ana_$count2.log -e Log/job_Ana_$count2.err run_Ana_$count2.csh
          mv run_Ana_$count2.csh script/. 
       fi
 
