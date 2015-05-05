7415269b5815a2d6a5746f439133ebbe20d7e709
kunsuoh <kunsuoh@gmail.com>
2015-05-05 14:35:21 +0900

    add specific file name

--- addHists.sh
+++ addHists.sh
@@ -4,6 +4,7 @@ for filename in $(cat fileLists/AnaList); do
 	if [ -f Out/$filename.picoNpe.root ]; then
     	filesize=$(wc -c "Out/$filename.picoNpe.hists.root" | cut -f 1 -d ' ')
 		if [ $filesize -gt 300 ]; then
+		echo "Adding Out/$filename.picoNpe.hists.root"
 	  		if [ $count -eq 0 ]; then 
 				hadd hists_tmp.root Out/$filename.picoNpe.hists.root
 				mv hists_tmp.root hists.root
@@ -14,3 +15,7 @@ for filename in $(cat fileLists/AnaList); do
    		fi
    fi
 done 
+
+now="$(date '+%Y%m%d_%H%M%S')"
+mv hists.root hists_$now.root
+echo "Output histogram file: hists_$now.root"
